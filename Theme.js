da8180588b8e7453f8657d7171b0dc07b0b7bd0b
sususandi <sususandi.dev@gmail.com>
2024-03-03 13:32:27 +0630

    first commit

---
+++ Theme.js
@@ -0,0 +1,23 @@
+import { Dimensions } from 'react-native';
+
+export const COLORS = {
+    primary: "rgb(0,96,114)",//
+    secondary: 'rgb(0,96,114)',
+    accent: 'rgb(16,20,53)',
+
+    success: '#477970',
+    error: '#D36065',
+
+    black: "#131B2D",
+    // black1: "#4E5F64",
+    white: "#ffffff",
+    background: "rgb(7,7,20)",
+    grey: "#969493"
+}
+
+// const {width, height} = Dimensions.get('window');
+// export const SIZES = {
+//     base: 10,
+//     width,
+//     height
+// }
