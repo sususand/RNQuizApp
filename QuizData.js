f38e1be59f3aaccc4f5da95562b4685145276c93
sususandi <sususandi.dev@gmail.com>
2024-03-23 09:29:50 +0630

    commit basic version of rn-quiz

--- QuizData.js
+++ QuizData.js
@@ -1,17 +1,103 @@
 export default data = [
     {
-        question: "What should you do when approaching a yellow traffic light?",
-        options: ["Speed up and cross the intersection quickly","Come to a complete stop","Slow down and prepare to stop","Ignore the light and continue driving"],
-        correct_option: "Slow down and prepare to stop"
+        question: "What command is used to create a new React Native project using Expo CLI?",
+        options: [
+            "create-react-native-app",
+            "react-native init",
+            "expo init",
+            "npm init"],
+        correct_option: "expo init"
     },
     {
-        question: "What does a red octagonal sign indicate?",
-        options: ["Yield right of way","Stop and proceed when safe","Merge with traffic","No left turn allowed"],
-        correct_option: "Stop and proceed when safe"
+        question: "Which component is used to display a list of data in React Native?",
+        options: [
+            "ListView",
+            "FlatList",
+            "ScrollView",
+            "List"],
+        correct_option: "FlatList"
     },
     {
-        question: "What is the purpose of a crosswalk?",
-        options: ["A designated area for parking","A place to stop and rest","A path for pedestrians to cross the road","A location for U-turns"],
-        correct_option: "A path for pedestrians to cross the road"
+        question: "How do you handle user input in React Native?",
+        options: [
+            "Using TextInput component",
+            "Using Button component",
+            "Using TouchableOpacity component",
+            "Using Switch component"],
+        correct_option: "Using TextInput component"
     }
+    ,
+
+    {
+        question: "Which command is used to run a React Native app on an iOS simulator?",
+        options: [
+            "react-native run-android",
+            "react-native run-ios",
+            "expo start --ios",
+            "expo start --android"],
+        correct_option: "react-native run-ios"
+    }
+    ,
+
+    {
+        question: "How can you debug a React Native app?",
+        options: [
+            "Using console.log statements",
+            "Using Reactotron",
+            "Using the React Native Debugger",
+            "All of the above"],
+        correct_option: "All of the above"
+    }
+    ,
+    {
+        question: "Which of the following lifecycle methods is not available in React Native?",
+        options: [
+            "componentDidMount",
+            "componentWillMount",
+            "componentDidUpdate",
+            "componentWillUnmount"],
+        correct_option: "componentWillMount"
+    }
+    ,
+
+    {
+        question: "What is the purpose of the AsyncStorage module in React Native?",
+        options: [
+            "To store data in a local SQLite database",
+            "To store data in the device's file system",
+            "To store data in a secure encrypted format",
+            "To store data persistently on the device"],
+        correct_option: "To store data persistently on the device"
+    }
+    ,
+    {
+        question: "How do you navigate between screens in a React Native app?",
+        options: [
+            "Using the <Navigation> component",
+            "Using the <Router> component",
+            "Using React Navigation library",
+            "Using Redux"],
+        correct_option: "Using React Navigation library"
+    }
+    ,
+    {
+        question: "Which of the following is used to style components in React Native?",
+        options: [
+            "CSS",
+            "Flexbox",
+            "Inline styles",
+            "Tailwind CSS"],
+        correct_option: "Flexbox"
+    }
+    ,
+    {
+        question: "What is the purpose of the react-native-vector-icons library?",
+        options: [
+            "To add vector graphics to a React Native app",
+            "To add custom fonts and icons to a React Native app",
+            "To add animations to a React Native app",
+            "To add navigation features to a React Native app"],
+        correct_option: "To add custom fonts and icons to a React Native app"
+    }
+
 ]
